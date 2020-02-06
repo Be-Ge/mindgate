e6a89ed3e18cc3f3b459cfa9b4cbcc24ba02f3cf
Anastasiia Buryak <be.ge.instagram@gmail.com>
2020-02-06 13:49:43 +0200

    add burger action

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const burger = document.querySelector('.burger'),
-    header = document.querySelector('.header');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const burger = document.querySelector('.burger'),
+//     header = document.querySelector('.header');
 
-    burger.addEventListener('click',  () => {
-        container.classList.toggle('menu-opened');
-    })
-})
+//     burger.addEventListener('click',  () => {
+//         container.classList.toggle('menu-opened');
+//     })
+// })
+
+const burgerOpen = document.querySelector('.burger-open');
+const burgerClose = document.querySelector('.burger-close');
+const windowMenu = document.querySelector('.window-menu');
+
+burgerOpen.onclick = (e) => windowMenu.classList.add('show');
+burgerClose.onclick = (e) => windowMenu.classList.delete('hidden');
