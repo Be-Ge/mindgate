4ad3cbcf72bc0b3b91f30248de0c1d6f45686c8b
Анастасия Сергеевна Буряк <buryak.a@msgroup.local>
2020-02-11 13:09:06 +0200

    add overflow

--- js/main.js
+++ js/main.js
@@ -11,5 +11,11 @@ const burgerOpen = document.querySelector('.burger-open');
 const burgerClose = document.querySelector('.burger-close');
 const windowMenu = document.querySelector('.window-menu');
 
-burgerOpen.onclick = (e) => windowMenu.classList.add('show');
-burgerClose.onclick = (e) => windowMenu.classList.remove('show');
+burgerOpen.onclick = (e) => {
+  windowMenu.classList.add('show')
+  document.body.classList.add('overflow')
+};
+burgerClose.onclick = (e) => {
+  windowMenu.classList.remove('show')
+  document.body.classList.remove('overflow')
+};
